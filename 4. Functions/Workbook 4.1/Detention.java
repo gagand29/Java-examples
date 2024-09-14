e8412bd69055ad09072102dd894aa10b1ea003ce
gagand29 <47719899+gagand29@users.noreply.github.com>
2024-09-14 13:39:16 -0500

    functions examples

---
+++ 4. Functions/Workbook 4.1/Detention.java
@@ -0,0 +1,20 @@
+public class Detention {
+    public static void main(String[] args) {
+
+       
+     printlines();
+     printlines();
+
+   }
+
+
+
+    public static void printlines(){
+
+      System.out.println("I will not copy and paste code.");
+      System.out.println("I will use Camel Case when writing class names.");
+      System.out.println("I will use lower Camel Case when writing function names.");
+      System.out.println("I will use lower Camel Case when writing variables names.\n");
+    }
+
+}
