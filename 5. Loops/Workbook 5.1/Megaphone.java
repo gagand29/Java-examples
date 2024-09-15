6a26cc0d2993ae0384840c38e08339fca7527920
gagand29 <47719899+gagand29@users.noreply.github.com>
2024-09-15 11:17:27 -0500

    completed loops (for and while) - thanks

---
+++ 5. Loops/Workbook 5.1/Megaphone.java
@@ -0,0 +1,10 @@
+public class Megaphone {
+    public static void main(String[] args) {
+        
+        for(int i=0; i<10;i++){
+            System.out.println(i+1 +"If Java was easy, they would call it Python!");
+        }
+
+    }
+    
+}
---
+++ 5. Loops/Workbook 5.7/FizzBuzz.java
@@ -0,0 +1,20 @@
+public class FizzBuzz {
+    public static void main(String[] args) {
+        for(int i = 0; i <19;i++){
+            if(i%3==0 && i%5==0){
+                System.out.println(i+"coca-cola");
+            }
+            else if(i%3==0) {
+                System.out.println(i+"coca");
+            }
+            else if(i%5==0){
+                System.out.println(i+"cola");
+            }
+
+            }
+        }
+      
+
+    }
+
+
