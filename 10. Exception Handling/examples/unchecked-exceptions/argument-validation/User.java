ca5aa46166fbede006eba8704e1c04ed84b45f5a
gagand29 <47719899+gagand29@users.noreply.github.com>
2024-09-25 02:59:32 -0500

    exception handling

---
+++ 10. Exception Handling/examples/unchecked-exceptions/argument-validation/User.java
@@ -0,0 +1,30 @@
+public class User {
+
+    private String username;
+    private int age;
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) { 
+        if(age < 0)
+        {
+            throw new IllegalArgumentException("Age must be positive");
+        }
+        this.age = age;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        if(username == null ||  username.isBlank()){
+            throw new IllegalArgumentException("Username cannot be empty");
+        }
+        this.username = username;
+    }
+
+
+}
