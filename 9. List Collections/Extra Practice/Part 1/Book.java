374cc84ff0581fec39fadb96500d9ace9bbb5859
gagand29 <47719899+gagand29@users.noreply.github.com>
2024-09-25 02:10:04 -0500

    extra prac

---
+++ 9. List Collections/Extra Practice/Part 1/Book.java
@@ -0,0 +1,71 @@
+public class Book {
+    private String title;
+    private String author;
+    private double rating;
+    private double price;
+    
+
+
+    public Book(String title, String author, double rating, double price) {
+        this.title = title;
+        this.author = author;
+        this.rating = rating;
+        this.price = price;
+    }
+
+    
+
+    public Book(Book Source) {
+        this.title = Source.title;
+        this.author = Source.author;
+        this.rating = Source.rating;
+        this.price = Source.price;
+    }
+
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getRating() {
+        return this.rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+
+
+    
+
+    public String toString() {
+        return "Book{" +
+                "Title='" + title + '\'' +
+                ", Author='" + author + '\'' +
+                ", Rating='" + rating + '\'' +
+                ", Price='" + price + '\'' +
+                '}';
+    }
+    
+}
